58bb3d669456a223a61bc6cf1b7e2aabb659d055
Stathis69 <147056555+Stathis69@users.noreply.github.com>
2026-01-10 23:55:38 +0200

    Add files via upload

---
+++ project/order.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+$cart_summary = "";
+if (isset($_SESSION['cart_summary']) && !empty($_SESSION['cart_summary'])) {
+$products_list = [
+        1 => "Κουραμπιέδες",
+        2 => "Μελομακάρονα",
+        3 => "Εκλεράκια",
+        4 => "Πάστες"
+    ];
+foreach ($_SESSION['cart_summary'] as $id => $qty) {
+        if (isset($products_list[$id])) {
+            $cart_summary .= $products_list[$id] . " (x" . $qty . ") ";
+        }
+    }
+} else {
+    $cart_summary = "Καμία παραγγελία προϊόντων";
+}
+
+
+$firstname = $_POST['firstname'];
+$lastname  = $_POST['lastname'];
+$address   = $_POST['address'];
+$phone     = $_POST['phone'];
+$date      = $_POST['date'];
+$email     = $_POST['email'];
+
+ $DB_SERVER = "localhost";
+ $DB_NAME = "zaxaroplasteio";
+ $DB_USER = "root";
+ $DB_PASSWORD = "";
+ $dblink = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME)
+ or die('Unable to connect to database!');
+  $sql_query = "INSERT INTO `orders` (`firstname`, `lastname`, `address`, `phone`, `date`,`email`,`product`)";
+ $sql_query .= " VALUES ('{$firstname}', '{$lastname}', '{$address}', '{$phone}', '{$date}','{$email}','{$cart_summary}')";
+ $success = mysqli_query($dblink, $sql_query);
+ if ($success){
+	 echo "<h2>Η παραγγελία σας καταχωρήθηκε επιτυχώς!</h2>";
+        echo "<p>Σύντομα θα λάβετε email επιβεβαίωσης ".htmlspecialchars($email).".</p>";
+		unset($_SESSION['cart']);
+		} else {
+    echo "Σφάλμα κατά την αποθήκευση: " . mysqli_error($dblink);
+}
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+ <br/>
+<td> <a href="proionta.html">Επιστροφή στα Προϊόντα</a> </td>
+</body>
+</html>
